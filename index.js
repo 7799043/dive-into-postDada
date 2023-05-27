2e433bf0a01ec60dea54c22ff7bd80b914b1fc01
7799043 <leoniak.maciek1@gmail.com>
2023-05-27 08:36:07 +0200

    newUser post

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/login', (req, res) => {
     res.sendFile("pages/login.html", { root: __dirname })
 })
 // Endpoints for APIs
-app.post('/login', async (req, res) => {
+app.post('/submit', async (req, res) => {
     let user = await User.findOne(req.body)
     console.log(user)
     if (!user) {
@@ -28,6 +28,15 @@ app.post('/login', async (req, res) => {
     }
     // res.sendFile("pages/signup.html", { root: __dirname })
 })
+
+app.post('/newUser', async (req, res) => {
+    const { userToken } = req.body
+    console.log(req.body)
+    let user = await User.create(req.body)
+    res.status(200).json({ success: true, user: user })
+})
+
+
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}/login`)
 })
