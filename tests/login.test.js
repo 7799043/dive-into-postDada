91d133c62024db39f8f72a1d1e7e843ee4802747
7799043 <leoniak.maciek1@gmail.com>
2023-06-14 21:56:05 +0200

    sth wrong

--- tests/login.test.js
+++ tests/login.test.js
@@ -2,20 +2,23 @@ const request = require('supertest');
 const app = require('../index');
 const User = require('../models/User');
 
-describe('POST /newUser', () => {
+describe('POST /newUser',  () => {
   it('should create a new user', async () => {
-    const newUser = {
-      email: 'test@example.com',
-      password: 'test123',
-    };
+  
 
-    const response = await request(app)
+    const response =  await request(app)
       .post('/newUser')
-      .send(newUser);
+      .send({ email: 'bu@ba.com', password: 'fatima' });
 
     expect(response.status).toBe(200);
     expect(response.body.success).toBe(true);
     expect(response.body.user.email).toBe(newUser.email);
     expect(response.body.user.password).toBe(newUser.password);
+
+    const createdUser = await User.findOne({ email: newUser.email });
+    expect(createdUser).toBeTruthy();
+
+    
+    await new Promise((resolve) => setTimeout(resolve, 15000));
   });
 });
